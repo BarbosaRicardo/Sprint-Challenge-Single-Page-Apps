42a143628b93706bff152f4095b39ac662ea15d2
Ricardo Barbosa <ric3639@gmail.com>
2019-10-12 10:53:56 -0700

    Forgot to commit, worked rest of cards out

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
 import AppRouter from "./components/AppRouter"
+import TabNav from "./components/TabNav.js";
+// import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <TabNav />
       <AppRouter />
+      {/* <SearchForm /> */}
     </main>
   );
 }
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import CharacterList from './CharacterList'
-// import WelcomePage from './WelcomePage'
-// import LocationList from './LocationsList'
+import WelcomePage from './WelcomePage'
+import LocationsList from './LocationsList'
 import EpisodesList from './EpisodesList'
 
 export default function AppRouter() {
     return <div >
         <Switch>
+            <Route exact path='/' component ={WelcomePage} />
             <Route path = "/characters" component = {CharacterList} />
             <Route path ="/episodes" component = {EpisodesList} />
+            <Route path ="/locations" component ={LocationsList}/>
+            
+            <Route component={LocationsList}/>
+            <Route component = {WelcomePage} />
             <Route component = {CharacterList} />
             <Route component = {EpisodesList} />
         </Switch>
